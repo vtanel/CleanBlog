93658b6af4bf667fe83857285e07014e09ea3945
Tanel Viira <tanel.viira@khk.ee>
2015-10-12 15:51:02 +0300

    1444622-105077628 config repair

--- include/database.php
+++ include/database.php
@@ -1,4 +1,5 @@
 <?php
+require 'config.php';
 //db connect
 $db = mysqli_connect(DATABASE_HOSTNAME, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_DATABASE) or die(mysqli_error($db));
 mysqli_query($db, "SET NAMES 'utf8'");
